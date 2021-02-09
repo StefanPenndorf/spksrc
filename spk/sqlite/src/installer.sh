d2ae7e8d0a0ffcbf88b84ef4508baf14675705cb
sadmin <sadmin@KaliStation.home>
2021-02-09 15:19:24 +0100

    Additional update SQLite DSM 7.0

--- spk/sqlite/src/installer.sh
+++ spk/sqlite/src/installer.sh
@@ -5,7 +5,7 @@ PACKAGE="redis"
 DNAME="Redis"
 
 # Others
-INSTALL_DIR="/usr/local/${PACKAGE}"
+INSTALL_DIR="/var/packages/${PACKAGE}/target"
 SSS="/var/packages/${PACKAGE}/scripts/start-stop-status"
 PATH="${INSTALL_DIR}/bin:${PATH}"
 TMP_DIR="${SYNOPKG_PKGDEST}/../../@tmp"
@@ -32,80 +32,28 @@ preinst ()
 
 postinst ()
 {
-    # Link
-    ln -s ${SYNOPKG_PKGDEST} ${INSTALL_DIR}
-
     # Set the maximum memory to use in configuration file
     sed -i -e "s/@maxmemory@/${MEMORY}mb/g" ${CFG_FILE}
-
-    # Install busybox stuff
-    ${INSTALL_DIR}/bin/busybox --install ${INSTALL_DIR}/bin
-
-    # Create legacy user
-    if [ "${BUILDNUMBER}" -lt "7321" ]; then
-        adduser -h ${INSTALL_DIR}/var -g "${DNAME} User" -G ${LEGACY_GROUP} -s /bin/sh -S -D ${LEGACY_USER}
-    fi
-
-    # Correct the files ownership
-    chown -R ${USER}:root ${SYNOPKG_PKGDEST}
 
     exit 0
 }
 
 preuninst ()
 {
-    # Stop the package
-    ${SSS} stop > /dev/null
-
-    if [ "${SYNOPKG_PKG_STATUS}" != "UPGRADE" ]; then
-        # Remove the user (if not upgrading)
-        delgroup ${LEGACY_USER} ${LEGACY_GROUP}
-        deluser ${USER}
-
-        # Remove firewall configuration
-        ${SERVICETOOL} --remove-configure-file --package ${PACKAGE}.sc >> /dev/null
-    fi
-
     exit 0
 }
 
 postuninst ()
 {
-    # Remove link
-    rm -f ${INSTALL_DIR}
-
     exit 0
 }
 
 preupgrade ()
 {
-    # Stop the package
-    ${SSS} stop > /dev/null
-
-    # DSM6 Upgrade handling
-    if [ "${BUILDNUMBER}" -ge "7321" ] && [ ! -f ${DSM6_UPGRADE} ]; then
-        echo "Deleting legacy user" > ${DSM6_UPGRADE}
-        delgroup ${LEGACY_USER} ${LEGACY_GROUP}
-        deluser ${LEGACY_USER}
-    fi
-
-    # Save some stuff
-    rm -fr ${TMP_DIR}/${PACKAGE}
-    mkdir -p ${TMP_DIR}/${PACKAGE}
-    mv ${INSTALL_DIR}/var ${TMP_DIR}/${PACKAGE}/
-
     exit 0
 }
 
 postupgrade ()
 {
-    # Restore some stuff
-    rm -fr ${INSTALL_DIR}/var
-    mv ${TMP_DIR}/${PACKAGE}/var ${INSTALL_DIR}/
-    rm -fr ${TMP_DIR}/${PACKAGE}
-
-    # Ensure file ownership is correct after upgrade
-    chown -R ${USER}:root ${SYNOPKG_PKGDEST}
-
     exit 0
 }
