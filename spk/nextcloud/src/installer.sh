14ca5549f40485f7c1f3e166bc30967e0572faf2
sadmin <sadmin@KaliStation.home>
2021-03-02 23:23:53 +0100

    NextCloud for DSM 7.0

--- spk/nextcloud/src/installer.sh
+++ spk/nextcloud/src/installer.sh
@@ -4,7 +4,7 @@
 PACKAGE="nextcloud"
 
 # Others
-INSTALL_DIR="/usr/local/${PACKAGE}"
+INSTALL_DIR="/var/packages/${PACKAGE}/target"
 
 # New role for embedded PostgreSQL
 PG_USERNAME=${wizard_pg_username:=pgadmin}
@@ -17,19 +17,16 @@ preinst ()
 
 postinst ()
 {
-     # Link
-     ln -s ${SYNOPKG_PKGDEST} ${INSTALL_DIR}
-
      # Embedded PostgreSQL local access
-     echo "host    all             all             127.0.0.1/24            md5" >>/etc/postgresql/pg_hba.conf
+     #echo "host    all             all             127.0.0.1/24            md5" >>/etc/postgresql/pg_hba.conf
 
      # Embedded PostgreSQL new role to have enough access rights for NextCloud
-     su - postgres -s /bin/sh -c "/usr/bin/psql -d postgres -c \"CREATE ROLE ${PG_USERNAME} ENCRYPTED PASSWORD '${PG_PASSWORD}' SUPERUSER CREATEDB CREATEROLE INHERIT LOGIN REPLICATION; \""
+     /usr/bin/psql -d postgres -c "CREATE ROLE ${PG_USERNAME} ENCRYPTED PASSWORD '${PG_PASSWORD}' SUPERUSER CREATEDB CREATEROLE INHERIT LOGIN REPLICATION; "
 
      # Web directory
-     cp -R ${INSTALL_DIR}/share/nextcloud/ /var/services/web/${PACKAGE}/
-     chown -R http:http /var/services/web/${PACKAGE}/
-     chmod -R 0770 /var/services/web/${PACKAGE}/
+     #cp -R ${INSTALL_DIR}/share/nextcloud/ /var/services/web/${PACKAGE}/
+     #chown -R http:http /var/services/web/${PACKAGE}/
+     #chmod -R 0770 /var/services/web/${PACKAGE}/
 
      exit 0
 }
@@ -42,17 +39,11 @@ preuninst ()
 postuninst ()
 {
 
-     # Web directory
-     rm -rf /var/services/web/${PACKAGE}/
-
      # Remove embedded PostgreSQL local access
-     sed -i '/host    all             all             127.0.0.1\/24            md5/d'  /etc/postgresql/pg_hba.conf
+     #sed -i '/host    all             all             127.0.0.1\/24            md5/d'  /etc/postgresql/pg_hba.conf
 
      # Drop embedded PostgreSQL new role 
-     su - postgres -s /bin/sh -c "/usr/bin/psql -d postgres -c \"DROP ROLE ${PG_USERNAME}; \""
-
-     # Link
-     rm -f ${INSTALL_DIR}
+     /usr/bin/psql -d postgres -c "DROP ROLE ${PG_USERNAME};"
 
      exit 0
 }
