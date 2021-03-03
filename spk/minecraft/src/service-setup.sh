f482aed7bc15e19a1325ffd618cc1eb28421aeef
sadmin <sadmin@KaliStation.home>
2021-03-03 19:29:58 +0100

    Minecraft : Fix server start/stop

--- spk/minecraft/src/service-setup.sh
+++ spk/minecraft/src/service-setup.sh
@@ -1,11 +1,13 @@
-PATH="${SYNOPKG_PKGDEST}:${PATH}"
-SERVICE_COMMAND="/usr/local/bin/java -Xmx1024M -Xms1024M -jar ${SYNOPKG_PKGDEST}/server.jar nogui"
+SERVICE_COMMAND="java -Xmx1024M -Xms1024M -jar ${SYNOPKG_PKGDEST}/server.jar nogui"
 SVC_BACKGROUND=y
 SVC_WRITE_PID=y
+SVC_CWD=${SYNOPKG_PKGHOME}
+
+MOJANG_JAR_LOCATION=https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar
 
 service_postinst ()
 {
-  curl -L -o ${SYNOPKG_PKGDEST}/server.jar https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar
+  curl -L -o ${SYNOPKG_PKGDEST}/server.jar ${MOJANG_JAR_LOCATION}
 
   # Copy eula.txt to user's home
   cp ${SYNOPKG_PKGDEST}/home/eula.txt ${SYNOPKG_PKGHOME}/
