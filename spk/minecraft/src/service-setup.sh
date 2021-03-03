451617e36dd85c79ff28fdc6406b36a72ab8f7ec
sadmin <sadmin@KaliStation.home>
2021-03-03 18:29:59 +0100

    Minecraft : store files in SYNOPKG_PKGHOME

--- spk/minecraft/src/service-setup.sh
+++ spk/minecraft/src/service-setup.sh
@@ -6,5 +6,11 @@ SVC_WRITE_PID=y
 service_postinst ()
 {
   curl -L -o ${SYNOPKG_PKGDEST}/server.jar https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar
+
+  # Copy eula.txt to user's home
+  cp ${SYNOPKG_PKGDEST}/home/eula.txt ${SYNOPKG_PKGHOME}/
+
+  # Copy server.properties as well
+  cp ${SYNOPKG_PKGDEST}/home/server.properties ${SYNOPKG_PKGHOME}/
 }
 
