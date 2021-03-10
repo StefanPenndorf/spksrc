052468bee4320cb5c202126c88dd0b544af373c5
sadmin <sadmin@KaliStation.home>
2021-03-10 23:55:35 +0100

    Nextcloud : remove WIZARD

--- spk/nextcloud/src/installer.sh
+++ spk/nextcloud/src/installer.sh
@@ -7,8 +7,8 @@ PACKAGE="nextcloud"
 INSTALL_DIR="/var/packages/${PACKAGE}/target"
 
 # New role for embedded PostgreSQL
-PG_USERNAME=${wizard_pg_username:=pgadmin}
-PG_PASSWORD=${wizard_pg_password:=changepassword}
+#PG_USERNAME=${wizard_pg_username:=pgadmin}
+#PG_PASSWORD=${wizard_pg_password:=changeme}
 
 preinst ()
 {
@@ -21,12 +21,7 @@ postinst ()
      #echo "host    all             all             127.0.0.1/24            md5" >>/etc/postgresql/pg_hba.conf
 
      # Embedded PostgreSQL new role to have enough access rights for NextCloud
-     /usr/bin/psql -d postgres -c "CREATE ROLE ${PG_USERNAME} ENCRYPTED PASSWORD '${PG_PASSWORD}' SUPERUSER CREATEDB CREATEROLE INHERIT LOGIN REPLICATION; "
-
-     # Web directory
-     #cp -R ${INSTALL_DIR}/share/nextcloud/ /var/services/web/${PACKAGE}/
-     #chown -R http:http /var/services/web/${PACKAGE}/
-     #chmod -R 0770 /var/services/web/${PACKAGE}/
+     #/usr/bin/psql -d postgres -c "CREATE ROLE ${PG_USERNAME} ENCRYPTED PASSWORD '${PG_PASSWORD}' SUPERUSER CREATEDB CREATEROLE INHERIT LOGIN REPLICATION; "
 
      exit 0
 }
@@ -39,11 +34,8 @@ preuninst ()
 postuninst ()
 {
 
-     # Remove embedded PostgreSQL local access
-     #sed -i '/host    all             all             127.0.0.1\/24            md5/d'  /etc/postgresql/pg_hba.conf
-
      # Drop embedded PostgreSQL new role 
-     /usr/bin/psql -d postgres -c "DROP ROLE ${PG_USERNAME};"
+     #/usr/bin/psql -d postgres -c "DROP ROLE ${PG_USERNAME};"
 
      exit 0
 }
