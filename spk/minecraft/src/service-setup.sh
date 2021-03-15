bbe2bd72b926a30da340c36e2b61ebd4cfe5ed1e
sadmin <sadmin@KaliStation.home>
2021-03-15 20:03:10 +0100

    Minecraft : enable save world before uninstall

--- spk/minecraft/src/service-setup.sh
+++ spk/minecraft/src/service-setup.sh
@@ -5,6 +5,9 @@ SVC_CWD=${SYNOPKG_PKGHOME}
 
 MOJANG_JAR_LOCATION=https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar
 
+WORLD_BACKUP="/tmp/${SYNOPKG_PKGNAME}_world.`date +\"%d-%b\"`.bak"
+UPGRADE_FILES="server.properties *.txt world *.json"
+
 service_postinst ()
 {
   curl -L -o ${SYNOPKG_PKGDEST}/server.jar ${MOJANG_JAR_LOCATION}
@@ -14,5 +17,23 @@ service_postinst ()
 
   # Copy server.properties as well
   cp ${SYNOPKG_PKGDEST}/home/server.properties ${SYNOPKG_PKGHOME}/
+}
+
+service_preuninst ()
+{
+
+  echo "service_preuninst ${SYNOPKG_PKG_STATUS}" >> ${SYNOPKG_TEMP_LOGFILE}
+
+  if [ ${wizard_keep_data} == "true" ]; then
+    #if a world exists, back it up to the tmp folder
+    if [ -d ${SYNOPKG_PKGHOME}/world ]; then
+      if [ ! -d ${WORLD_BACKUP} ]; then
+        mkdir -p ${WORLD_BACKUP}
+      fi
+      for ITEM in ${UPGRADE_FILES}; do
+        cp -r ${SYNOPKG_PKGHOME}/${ITEM} ${WORLD_BACKUP}
+      done
+    fi
+  fi
 }
 
