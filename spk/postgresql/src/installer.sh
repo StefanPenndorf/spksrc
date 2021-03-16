6d32f7e76479e75d1312e25dd351ea13ba91bf01
sadmin <sadmin@KaliStation.home>
2021-03-16 18:59:45 +0100

    PostgreSQL : tentative to propose backup before uninstalling package

--- spk/postgresql/src/installer.sh
+++ spk/postgresql/src/installer.sh
@@ -18,6 +18,14 @@ PG_USERNAME=${wizard_pg_username:=pgadmin}
 PG_PASSWORD=${wizard_pg_password:=changepassword}
 PG_PORT=${wizard_pg_port:=5433}
 
+PG_BACKUP=${wizard_pg_dump_directory}
+PG_BACKUP_CONF_DIR="${PG_BACKUP}/conf_`date +\"%d_%b\"`"
+PG_BACKUP_DUMP_DIR="${PG_BACKUP}/databases_`date +\"%d_%b\"`"
+PG_BACKUP_DUMP_FILE_SUFFIX=".dump"
+
+INST_ETC="/var/packages/${SYNOPKG_PKGNAME}/etc"
+INST_VARIABLES="${INST_ETC}/installer-variables"
+
 preinst ()
 {
     exit 0
@@ -43,11 +51,51 @@ postinst ()
   # Stop server
   ${SYNOPKG_PKGDEST}/bin/pg_ctl -D ${DATABASE_DIR} stop
 
+  echo "SAVE_PG_USERNAME=${wizard_pg_username}" > ${INST_VARIABLES}
+  echo "SAVE_PG_PASSWORD=${wizard_pg_password}" >> ${INST_VARIABLES}
+  echo "SAVE_PG_PORT=${wizard_pg_port}" >> ${INST_VARIABLES}
+
   exit 0
 }
 
 preuninst ()
 {
+
+  # Backup only on user's request
+  if [ "$wizard_pg_dump_database" = "true" ]; then
+
+    # Start server again
+    ${SYNOPKG_PKGDEST}/bin/pg_ctl -D ${DATABASE_DIR} -l ${DATABASE_DIR}/logfile start
+
+    # Create backup conf dir
+    if [ ! -d ${PG_BACKUP_CONF_DIR} ]; then
+      mkdir -p ${PG_BACKUP_CONF_DIR}
+      chmod 755 ${PG_BACKUP_CONF_DIR}
+    fi
+
+    # Backup config files
+    cp ${DATABASE_DIR}/pg_hba.conf ${PG_BACKUP_CONF_DIR}/
+    cp ${DATABASE_DIR}/pg_ident.conf ${PG_BACKUP_CONF_DIR}/
+    cp ${DATABASE_DIR}/postgresql.auto.conf ${PG_BACKUP_CONF_DIR}/
+    cp ${DATABASE_DIR}/postgresql.conf ${PG_BACKUP_CONF_DIR}/
+
+    # Create backup dump dir
+    if [ ! -d ${PG_BACKUP_DUMP_DIR} ]; then
+      mkdir -p ${PG_BACKUP_DUMP_DIR}
+      chmod 777 ${PG_BACKUP_DUMP_DIR}
+    fi
+
+    # Backup all databases before uninstalling
+    for database in `${SYNOPKG_PKGDEST}/bin/psql -A -t -p ${PG_PORT} -U ${PG_USERNAME} -d postgres -c "select datname from pg_database"`;
+    do
+            # Dumping each database
+            ${SYNOPKG_PKGDEST}/bin/pg_dump -p ${PG_PORT} -U ${PG_USERNAME} -Fc ${database} >${PG_BACKUP_DUMP_DIR}/${database}${PG_BACKUP_DUMP_FILE_SUFFIX}
+    done
+
+    # Stop server
+    ${SYNOPKG_PKGDEST}/bin/pg_ctl -D ${DATABASE_DIR} stop
+  fi
+
   exit 0
 }
 
