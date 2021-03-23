70b1436abd96b3d97194bd3c8802a895bd25f75b
sadmin <sadmin@KaliStation.home>
2021-03-23 19:50:05 +0100

    Terraria : add world and start/stop

--- spk/terraria-mobile/src/service-setup.sh
+++ spk/terraria-mobile/src/service-setup.sh
@@ -1,4 +1,6 @@
-#SERVICE_COMMAND="${SYNOPKG_PKGDEST}/ServerLinux/TerrariaServer.bin.x86_64 -port 7777 -maxplayers 16 -world ${SYNOPKG_PKGHOME}/.local/share/Terraria/Mobile/Worlds/SynoWorld.wld"
+SERVICE_COMMAND="screen -dmS terraria /bin/bash -c \"${SYNOPKG_PKGDEST}/ServerLinux/TerrariaServe
+r.bin.x86_64 -port 7777 -maxplayers 16 -world ${SYNOPKG_PKGHOME}/.local/share/Terraria/Mobile/Wor
+lds/SynoWorld.wld\""
 SVC_BACKGROUND=n
 SVC_WRITE_PID=y
 SVC_CWD=${SYNOPKG_PKGHOME}
@@ -9,6 +11,16 @@ service_postinst ()
 {
   # Download server
   #curl -L -o ${SYNOPKG_PKGDEST}/MobileTerrariaServer.zip ${SERVER_ZIP_LOCATION}
+
+  # Create Worlds folder 
+  mkdir ${SYNOPKG_PKGHOME}/.local
+  mkdir ${SYNOPKG_PKGHOME}/.local/share
+  mkdir ${SYNOPKG_PKGHOME}/.local/share/Terraria
+  mkdir ${SYNOPKG_PKGHOME}/.local/share/Terraria/Mobile
+  mkdir ${SYNOPKG_PKGHOME}/.local/share/Terraria/Mobile/Worlds/
+
+  # Copy world
+  cp ${SYNOPKG_PKGDEST}/SynoWorld.wld ${SYNOPKG_PKGHOME}/.local/share/Terraria/Mobile/Worlds/
 
 }
 
