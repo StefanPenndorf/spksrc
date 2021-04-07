edc02591c3590327775483c4ae017f90986b5cf0
DigitalBox94 <nmougel@gmail.com>
2021-04-07 07:19:42 +0000

    Snek : fix start/stop

--- spk/snek/src/service-setup.sh
+++ spk/snek/src/service-setup.sh
@@ -1,6 +1,17 @@
 SERVICE_COMMAND="npm start"
 SVC_BACKGROUND=y
 SVC_WRITE_PID=y
-SVC_CWD=${SYNOPKG_PKGHOME}
+SVC_CWD="${SYNOPKG_PKGDEST}"
 
+service_postinst ()
+{
+	# Install for NodeJS
+	cd ${SYNOPKG_PKGDEST}
+	npm install
+
+	# Copy JS file to correct directory for running
+	cp ${SYNOPKG_PKGDEST}/node_modules/socket.io-client/dist/socket.io.js ${SYNOPKG_PKGDEST}/node_modules/socket.io-client/
+
+	echo "Installation completed"
+}
 
