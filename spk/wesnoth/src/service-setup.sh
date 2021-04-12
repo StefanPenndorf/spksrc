0c2e3808c820cdc339fb7342ca1f27e44e2b38e5
DigitalBox94 <nmougel@gmail.com>
2021-04-12 22:01:42 +0000

    Wesnoth : add config file and wizard

--- spk/wesnoth/src/service-setup.sh
+++ spk/wesnoth/src/service-setup.sh
@@ -1,7 +1,13 @@
-SERVICE_COMMAND="./wesnothd -p 15001"
-SVC_CWD="${SYNOPKG_PKGDEST}/usr/local/bin/"
+CFG_FILE="${SYNOPKG_PKGDEST}/var/wesnoth.cfg"
+SERVICE_COMMAND="${SYNOPKG_PKGDEST}/usr/local/bin/wesnothd -p 15001 -c ${CFG_FILE}"
+SVC_CWD="${SYNOPKG_PKGHOME}"
 SVC_BACKGROUND=y
 SVC_WRITE_PID=y
 
 
+service_postinst ()
+{
+    # Edit the configuration according to the wizard
+    sed -i -e "s/@password@/${wizard_wn_password:=changeme}/g" ${CFG_FILE}
+}
 
