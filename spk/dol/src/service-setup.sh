3060e9a55a47593b8a00c29c630962bef00b28dc
DigitalBox94 <nmougel@gmail.com>
2021-07-19 14:08:05 +0000

    DOLServer : create database during first installation

--- spk/dol/src/service-setup.sh
+++ spk/dol/src/service-setup.sh
@@ -10,6 +10,14 @@ service_postinst ()
     # Edit the configuration according to the wizard
     sed -i -e "s/@login@/${wizard_sql_username:=root}/g" ${CFG_FILE}
     sed -i -e "s/@password@/${wizard_sql_password:=changepassword}/g" ${CFG_FILE}
+    sed -i -e "s/@port@/${wizard_sql_port:=3306}/g" ${CFG_FILE}
+    sed -i -e "s/@ip_address@/${wizard_ip_address}/g" ${CFG_FILE}
+
+    # Create dol database if not exists
+    mysql -u ${wizard_sql_username:=root} -p${wizard_sql_password:=changepassword} -P ${wizard_sql_port:=3306} -e 'create database if not exists dol'
+
+    # Fill database with data
+    mysql -u ${wizard_sql_username:=root} -p${wizard_sql_password:=changepassword} -P ${wizard_sql_port:=3306} -b dol < public-db.sql
 }
 
 
