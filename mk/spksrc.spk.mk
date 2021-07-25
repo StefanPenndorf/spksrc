ee2be90426c4230d52261dcc925bc144e3c4964f
DigitalBox94 <nmougel@gmail.com>
2021-07-25 17:13:11 +0000

    Fix on mk scripts

--- mk/spksrc.spk.mk
+++ mk/spksrc.spk.mk
@@ -34,8 +34,6 @@ SPK_NAME_ARCH = noarch
 ifeq ($(call version_ge, ${TCVERSION}, 7.0),1)
 SPK_TCVERS = dsm7
 OS_MIN_VER = 7.0-40000
-<<<<<<< HEAD
-=======
 else ifeq ($(call version_ge, ${TCVERSION}, 6.0),1)
 SPK_TCVERS = dsm6
 OS_MIN_VER = 6.0-7321
@@ -43,15 +41,11 @@ else
 ifeq ($(call version_ge, ${TCVERSION}, 6.0),1)
 SPK_TCVERS = dsm6
 OS_MIN_VER = 6.0-7321
->>>>>>> master
 else
 SPK_TCVERS = all
 OS_MIN_VER = 3.1-1594
 endif
-<<<<<<< HEAD
-=======
 endif
->>>>>>> master
 ARCH_SUFFIX = -$(SPK_TCVERS)
 FIRMWARE = $(OS_MIN_VER)
 endif
@@ -188,34 +182,15 @@ ifeq ($(RELOAD_UI),yes)
 	@echo reloadui=\"$(RELOAD_UI)\" >> $@
 endif
 
-<<<<<<< HEAD
-ifneq ($(call version_ge, ${TCVERSION}, 7.0),1)
-# old behaviour
-ifeq ($(STARTABLE),no)
-ifeq ($(call version_le, ${TC_OS_MIN_VER}, 6.1),1)
-=======
 # for non startable (i.e. non service, cli tools only)
 # as default is 'yes' we only add this value for 'no'
 ifeq ($(STARTABLE),no)
 ifeq ($(call version_ge, ${TCVERSION}, 6.1),1)
 	@echo ctl_stop=\"$(STARTABLE)\" >> $@
 else
->>>>>>> master
 	@echo startable=\"$(STARTABLE)\" >> $@
 endif
 endif
-<<<<<<< HEAD
-else
-# since 7.0 use Synology resource acquisition
-ifeq ($(STARTABLE),no)
-ifeq ($(strip $(SPK_COMMANDS)),)
-# STARTABLE needs to be yes, Resource linking and unlinking works on start and stop
-	@echo ctl_stop=\"$(STARTABLE)\" >> $@
-endif
-endif
-endif
-=======
->>>>>>> master
 
 	@echo displayname=\"$(DISPLAY_NAME)\" >> $@
 ifneq ($(strip $(DSM_UI_DIR)),)
@@ -332,15 +307,9 @@ ifneq ($(strip $(SPK_ICON)),)
 	$(create_target_dir)
 	@$(MSG) "Creating PACKAGE_ICON.PNG for $(SPK_NAME)"
 ifneq ($(call version_ge, ${TCVERSION}, 7.0),1)
-<<<<<<< HEAD
-	(convert $(SPK_ICON) -thumbnail 72x72 -strip - > $(WORK_DIR)/PACKAGE_ICON.PNG)
-else
-	(convert $(SPK_ICON) -thumbnail 64x64 -strip - > $(WORK_DIR)/PACKAGE_ICON.PNG)
-=======
 	(convert $(SPK_ICON) -resize 72x72 -strip -sharpen 0x2 - > $(WORK_DIR)/PACKAGE_ICON.PNG)
 else
 	(convert $(SPK_ICON) -resize 64x64 -strip -sharpen 0x2 - > $(WORK_DIR)/PACKAGE_ICON.PNG)
->>>>>>> master
 endif
 	@$(MSG) "Creating PACKAGE_ICON_256.PNG for $(SPK_NAME)"
 	(convert $(SPK_ICON) -resize 256x256 -strip -sharpen 0x2 - > $(WORK_DIR)/PACKAGE_ICON_256.PNG)
@@ -354,19 +323,6 @@ info-checksum:
 
 .PHONY: wizards
 wizards:
-<<<<<<< HEAD
-	@$(MSG) "Preparing DSM Wizards"
-	@mkdir -p $(DSM_WIZARDS_DIR)
-ifneq ($(strip $(WIZARDS_DIR)),)
-	@find $${SPKSRC_WIZARDS_DIR} -maxdepth 1 -type f -and \( -name "install_uifile" -or -name "install_uifile_???" -or -name "install_uifile.sh" -or -name "install_uifile_???.sh" -or -name "upgrade_uifile" -or -name "upgrade_uifile_???" -or -name "upgrade_uifile.sh" -or -name "upgrade_uifile_???.sh" -or -name "uninstall_uifile" -or -name "uninstall_uifile_???" -or -name "uninstall_uifile.sh" -or -name "uninstall_uifile_???.sh" \) -print -exec cp -f {} $(DSM_WIZARDS_DIR) \;
-endif
-ifeq ($(call version_ge, ${TCVERSION}, 7.0),1)
-	@find $(SPKSRC_MK)wizard -maxdepth 1 -type f -and \( -name "uninstall_uifile" -or -name "uninstall_uifile_???" \) -print -exec cp -f {} $(DSM_WIZARDS_DIR) \;
-endif
-	@find $(DSM_WIZARDS_DIR) -maxdepth 1 -type f -not -name "*.sh" -print -exec chmod 0644 {} \;
-	@find $(DSM_WIZARDS_DIR) -maxdepth 1 -type f -name "*.sh" -print -exec chmod 0755 {} \;
-	$(eval SPK_CONTENT += WIZARD_UIFILES)
-=======
 ifeq ($(call version_ge, ${TCVERSION}, 7.0),1)
 	@$(MSG) "Create default DSM7 uninstall wizard"
 	@mkdir -p $(DSM_WIZARDS_DIR)
@@ -388,7 +344,6 @@ ifeq ($(call version_ge, ${TCVERSION}, 7.0),1)
 	$(eval SPK_CONTENT += WIZARD_UIFILES)
 endif
 endif
->>>>>>> master
 
 .PHONY: conf
 conf:
